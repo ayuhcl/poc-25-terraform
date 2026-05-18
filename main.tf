8cc193e0eed65313a60f858fdb9b0d5e2a4bd4d9
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-19 00:20:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
-
 module "vpc" {
   source       = "./modules/vpc"
   cidr_block   = "10.0.0.0/16"
